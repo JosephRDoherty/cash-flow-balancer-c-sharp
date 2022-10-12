833453ba1e21aeed2b1d3c7af575638f99f53e48
Joseph Doherty <dohertyj@firemtn.com>
2022-10-12 16:28:18 -0700

    Add Javascript files

---
+++ cash-flow-balancer-c-sharp/cash-flow-balancer-c-sharp/wwwroot/js/cashFlowController.js
@@ -0,0 +1,8 @@
+// Controller for CashBalancerView.cshtml
+
+
+// This code should take user input from the view and feed the output to the view
+
+
+// Javascript frickin sucks so this "file" is currently inside cashFlowModel.js
+// Javascript sucks so much dude.
---
+++ cash-flow-balancer-c-sharp/cash-flow-balancer-c-sharp/wwwroot/js/cashFlowModel.js
@@ -0,0 +1,52 @@
+// Imported by cashFlowController.js (or at least it will be when I can make an import actually WORK)
+'use strict';
+// This file should do the calculations that get sent to and from the controller.
+// Essentially this file will provide a number of functions to be called by the controller.
+// 
+
+// income
+var period1 = 2648;
+var period2 = 1496;
+var totalIncome = period1 + period2;
+// expenses
+var duePeriod1 = 1232;
+var duePeriod2 = 2012;
+var totalExpenses = duePeriod1 + duePeriod2;
+
+//console.log(findPercent(totalIncome, totalExpenses));
+
+// find x is what percent of y,
+// percentOf is the typically larger number, percentage is the smaller number (the one that will evaluate to a percentage)
+function findPercent(percentOf, percentage){
+    let percentCost = (percentage / percentOf )*100;
+    return percentCost;
+}
+
+// Finds how much money remains after expences
+// defaults to totalIncome and totalExpenses for ease of use, but I'm adding this for future functionality
+function findProfit(income = totalIncome, expenses = totalExpenses){
+    let profit = income - expenses;
+    return profit;
+}
+
+
+function balancerPrototype(){
+
+}
+
+
+
+
+
+
+
+
+// cashFlowController.js
+//
+// Since javascript is terrible,
+// I can't just IMPORT A JS FILE like you can in EVERY OTHER LANGUAGE without all sorts of complicated BS that I don't feel like doing right now
+// So this is the beginning of cashFlowController.js, and I'll copy-paste this later when Javascript sucks less.
+// Seriously though why is Javascript so terrible
+
+console.log("Profit: $",findProfit(), findPercent(totalIncome, findProfit()), "%");
+
